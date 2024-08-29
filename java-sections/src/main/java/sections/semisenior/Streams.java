c8b47ca34b6b2239b35989f686af505fece0d08f
Jordan Larcher <lds.larcher@gmail.com>
2024-08-29 18:04:59 -0300

    completing the stream exercise

--- java-sections/src/main/java/sections/semisenior/Streams.java
+++ java-sections/src/main/java/sections/semisenior/Streams.java
@@ -1,8 +1,22 @@
 package sections.semisenior;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class Streams {
 
     public static void main(String[] args) {
+
+
+        List<Integer> myIntegerList = Arrays.asList(500,200,10,23,3,4,32,1,2,500, 5000, 5000, 5000);
+
+        myIntegerList.stream().distinct().forEach(System.out::println);
+
+        Optional<Integer> maxNumberInList = myIntegerList.stream().distinct().max(Comparator.naturalOrder());
+
+        maxNumberInList.ifPresent(thisMax -> System.out.println("The maximum number is: " + thisMax));
         
     }
 }
