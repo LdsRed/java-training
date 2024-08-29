2cdbbc079243db7ff193a3ee45a2857e4f5ac687
jor-test-pop-os <lds.larcher@gmail.com>
2024-08-29 10:42:11 -0300

    adding optional examples

--- java-sections/src/main/java/sections/semisenior/OptionalsExample.java
+++ java-sections/src/main/java/sections/semisenior/OptionalsExample.java
@@ -3,24 +3,34 @@ package sections.semisenior;
 import java.util.Optional;
 
 public class OptionalsExample {
-
     public static void main(String[] args) {
 
-        String name = "Jordan";
 
-        //Optional<String> optional = Optional.of(name);
-        //para manejar los nulls podemos usar Optiona.ofNullable
+        // 1) Crear un Optional a partir de un valor no nulo y verificar si está presente.
+
+        Optional<String> myOptional = Optional.of("Hello");
+        System.out.println(myOptional.isPresent());
+
+        // 2) Usar Optional para manejar un valor potencialmente nulo sin usar condicionales.
+        /*Nullable ayuda a manejar valores potencialmente nulos, y devuelve un optional vacio (empty)
+        * Cuando se le pasa un valor nulo ofNullable no hará nada */
+        String myNull = null;
+
+        Optional<String> myOptional2 = Optional.ofNullable(myNull);
+        System.out.println(myOptional2.isPresent()); // returns false
 
 
-        Optional<String> optionalNullable = Optional.ofNullable(name);
+        // 3) Implementar una operación que devuelva un valor predeterminado si el Optional está vacío.
+            // Valor por defaul con .orElse()
+        String name  = "Jordan Larcher";
+        String defaultName = Optional.ofNullable(name).orElseGet( () -> "John Doe");
 
-        if(optionalNullable.isPresent()){
-            System.out.println(optionalNullable.get());
-        }
+        System.out.println(defaultName);
 
-        System.out.println(optionalNullable.isPresent(nombre -> {
-            System.out.println(nombre);
-        }));
+
+
+
+
     }
 
 
